a4b67953947419f34ae21bfc34c67d2acf152497
Benyair Orellana <ben.orellana@gmail.com>
2019-03-11 21:20:00 -0400

    end of section 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'recipes', pathMatch: 'full' },
-  { path: 'recipes', loadChildren: './recipes/recipes.module#RecipesPageModule' },
-];
+  { path: 'recipes',
+    children : [
+      {
+        path: '',
+        loadChildren: './recipes/recipes.module#RecipesPageModule'
+      },
+      {
+        path: ':recipeId',
+        loadChildren: './recipes/recipe-detail/recipe-detail.module#RecipeDetailPageModule'
+      }
+    ]
+  }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

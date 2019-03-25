2f9b48224ff38f766d4c192246b9854189acf3fc
Benyair Orellana <ben.orellana@gmail.com>
2019-03-25 06:57:07 -0400

    section 10 part 2

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -11,6 +11,8 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
+
   constructor(private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) { }
 
   ngOnInit() {
@@ -31,8 +33,23 @@ export class AuthPage implements OnInit {
     this.authService.login();
   }
 
+  onSwitchAuthMode() {
+    this.isLogin = !this.isLogin;
+  }
+
   onSubmit(form: NgForm) {
-    console.log(form);
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if (this.isLogin) {
+      // Send a request to login servers
+    } else {
+      // Send a request to signup servers
+    }
   }
 
 }

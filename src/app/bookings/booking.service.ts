50467d79c6bf9d156a24164ffe30718be4285680
Benyair Orellana <ben.orellana@gmail.com>
2019-03-29 07:05:12 -0400

    Section 11 part 1 of Video 182

--- src/app/bookings/booking.service.ts
+++ src/app/bookings/booking.service.ts
@@ -1,12 +1,49 @@
 import { BehaviorSubject } from 'rxjs';
 import { Booking } from './booking.model';
 import { Injectable } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { take, tap, delay } from 'rxjs/operators';
 
-@Injectable({ providedIn: 'root'})
+@Injectable({ providedIn: "root" })
 export class BookingService {
-    private _bookings: Booking[] = new BehaviorSubject<Booking>([]) ;
+  private _bookings = new BehaviorSubject<Booking[]>([]);
 
-    get bookings() {
-        return [...this._bookings];
-    }
+  get bookings() {
+    return this._bookings.asObservable();
+  }
+
+  constructor(private authService: AuthService) {}
+
+  addBooking(
+    placeId: string,
+    placeTitle: string,
+    placeImage: string,
+    firstName: string,
+    lastName: string,
+    guestNumber: number,
+    dateFrom: Date,
+    dateTo: Date
+  ) {
+    const newBooking = new Booking(
+      Math.random().toString(),
+      placeId,
+      this.authService.userId,
+      placeTitle,
+      placeImage,
+      firstName,
+      lastName,
+      guestNumber,
+      dateFrom,
+      dateTo
+    );
+    return this.bookings.pipe(
+      take(1),
+      delay(1000),
+      tap(bookings => {
+        this._bookings.next(bookings.concat(newBooking));
+      })
+    );
+  }
+
+  cancelBooking() {}
 }
--- src/app/bookings/bookings.page.ts
+++ src/app/bookings/bookings.page.ts
@@ -1,20 +1,30 @@
 import { BookingService } from './booking.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Booking } from './booking.model';
 import { IonItemSliding } from '@ionic/angular';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-bookings',
   templateUrl: './bookings.page.html',
   styleUrls: ['./bookings.page.scss'],
 })
-export class BookingsPage implements OnInit {
+export class BookingsPage implements OnInit, OnDestroy {
   loadedBookings: Booking[];
+  private bookingSub: Subscription;
 
   constructor(private bookingsService: BookingService) { }
 
   ngOnInit() {
-    this.loadedBookings = this.bookingsService.bookings;
+    this.bookingSub = this.bookingsService.bookings.subscribe(bookings => {
+      this.loadedBookings = bookings;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.bookingSub) {
+      this.bookingSub.unsubscribe();
+    }
   }
 
   onCancelBooking(offerId: string, slidingEl: IonItemSliding) {

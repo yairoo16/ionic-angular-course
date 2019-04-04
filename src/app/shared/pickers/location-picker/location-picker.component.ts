95abb0b979af9b83aa763ee3bfe92ddd49b77313
Benyair Orellana <orellanab@michigan.gov>
2019-04-04 16:03:14 -0400

    section 13 part 2

--- src/app/shared/pickers/location-picker/location-picker.component.ts
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -1,29 +1,41 @@
+import { PlaceLocation } from './../../../places/loaction.model';
 import { Component, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { MapModalComponent } from '../map-modal/map-modal.component';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-location-picker',
   templateUrl: './location-picker.component.html',
-  styleUrls: ['./location-picker.component.scss'],
+  styleUrls: ['./location-picker.component.scss']
 })
 export class LocationPickerComponent implements OnInit {
-
-  constructor(private modalCtrl: ModalController, private http: HttpClient) { }
+  constructor(private modalCtrl: ModalController, private http: HttpClient) {}
 
   ngOnInit() {}
 
   onPickLocation() {
-    this.modalCtrl.create({component: MapModalComponent}).then(modalEl => {
+    this.modalCtrl.create({ component: MapModalComponent }).then(modalEl => {
       modalEl.onDidDismiss().then(modalData => {
         if (!modalData.data) {
           return;
         }
-        this.getAddress(modalData.data.lat, modalData.data.lng).subscribe(address => {
-          console.log(address);
+        const pickedLocation: PlaceLocation = {
+          lat: modalData.data.lat,
+          lng: modalData.data.lng,
+          address: null,
+          staticMapImageUl: null
+        };
+        this.getAddress(modalData.data.lat, modalData.data.lng).pipe(
+          switchMap(address => {
+            pickedLocation.address = address;
+            return of(this.getMapImage(pickedLocation.lat, pickedLocation.lng, 14));
+          })
+        ).subscribe(staticMapImageUrl => {
+          pickedLocation.staticMapImageUl = staticMapImageUrl;
         });
       });
       modalEl.present();
@@ -31,14 +43,23 @@ export class LocationPickerComponent implements OnInit {
   }
 
   private getAddress(lat: number, lng: number) {
-    return this.http.get<any>(
-      `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=
-      ${environment.googleMapsApiKey}`).pipe(map((geoData: any) => {
-        if (!geoData || !geoData.results || geoData.results.length === 0) {
-          return null;
-        }
-        return geoData.results[0].formatted_address;
-      }));
+    return this.http
+      .get<any>(
+        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=
+      ${environment.googleMapsApiKey}`
+      )
+      .pipe(
+        map((geoData: any) => {
+          if (!geoData || !geoData.results || geoData.results.length === 0) {
+            return null;
+          }
+          return geoData.results[0].formatted_address;
+        })
+      );
   }
 
+  private getMapImage(lat: number, lng: number, zoom: number) {
+    // tslint:disable-next-line:max-line-length
+    return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=500x400&key=${environment.googleMapsApiKey}`;
+  }
 }

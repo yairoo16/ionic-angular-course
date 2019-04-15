3a7b99c9ef57222d3c86a03a10097aeb9b1364c8
Benyair Orellana <orellanab@michigan.gov>
2019-04-15 13:47:12 -0400

    end of section 16

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  googleMapsApiKey: 'AIzaSyAi5hjRs6kRCKTt95eaJvZ5Vequ93Z03NA'
+  googleMapsApiKey: 'AIzaSyAi5hjRs6kRCKTt95eaJvZ5Vequ93Z03NA',
+  firebaseAPIKey: 'AIzaSyCeF44S5VcEap9dmjD3kZafgLnl0D4QPCw'
 };
